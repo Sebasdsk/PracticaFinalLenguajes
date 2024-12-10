e5dbdbb518ba3cc883e91464ce0533ff4e453801
Sebasdsk <80434951+Sebasdsk@users.noreply.github.com>
2024-12-09 21:38:32 -0700

    clases declaradas

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from enum import nonmember
+
+import _mysql_connector
+import mysql.connector
+from PySide6.QtWidgets import QApplication, QWidget,QMainWindow
 from Interfaces.ui_login import Ui_Login  # Importa la clase generada automáticamente
-from Interfaces.ui_menu import Ui_MainWindowMenu as Menu
-from Interfaces.ui_registro import Ui_FormRegistro
-from Interfaces.ui_juego import Ui_FormJuego
-from Interfaces.ui_estadistica import Ui_FormEstats
+from Interfaces.ui_menu import Ui_MainWindowMenu as menuPrincipal
+from Interfaces.ui_registro import Ui_FormRegistro as registro
+from Interfaces.ui_juego import Ui_FormJuego as juego
+from Interfaces.ui_estadistica import Ui_FormEstats as estats
+from Interfaces.ui_lobby import Ui_FormLobby as lobby
+
+# Conexion a la base de datos:
+def conectar_bd():
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Galletas123",
+            database="bd_juego"
+        )
+        return conexion
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
+
+
+
 
 class LoginWindow(QWidget):  # Hereda de QWidget, ya que tu archivo generado es un widget
     def __init__(self):
@@ -11,24 +33,37 @@ class LoginWindow(QWidget):  # Hereda de QWidget, ya que tu archivo generado es 
         self.ui = Ui_Login()
         self.ui.setupUi(self)  # Configura la interfaz en esta ventana
 
-        # Conecta los botones a funciones específicas
-        self.ui.btnLogin.clicked.connect(self.handle_login)
-        self.ui.btnRegistro.clicked.connect(self.handle_register)
-
-    def handle_login(self):
-        # Obtén los textos de los campos de usuario y contraseña
-        username = self.ui.textEdit.toPlainText()
-        password = self.ui.textEdit_2.toPlainText()
-        # Realiza alguna lógica de validación (ejemplo)
-        if username == "admin" and password == "1234":
-            print("Login exitoso")
-        else:
-            print("Usuario o contraseña incorrectos")
-
-    def handle_register(self):
-        print("Registro de nuevo usuario")
 
 
+class RegisterWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = registro()
+        self.ui.setupUi(self)
+
+class JuegoWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = juego()
+        self.ui.setupUi(self)
+
+class MenuPrincipal(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = menuPrincipal
+        self.ui.setupUi(self)
+
+class EstadisticaWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = estats
+        self.ui.setupUi(self)
+
+class LobbyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = lobby
+        self.ui.setupUi(self)
 
 
 
