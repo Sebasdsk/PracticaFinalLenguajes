bd9aada03d537ef6b8775e251bfbff0361874b9a
Sebasdsk <80434951+Sebasdsk@users.noreply.github.com>
2024-12-11 02:19:51 -0700

    Se hicieron modificaciones a la interfaz del lobby

--- Interfaces/ui_lobby.py
+++ Interfaces/ui_lobby.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'lobbyCtYrYw.ui'
+## Form generated from reading UI file 'lobbyvTnfng.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.0
 ##
@@ -15,14 +15,14 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QListWidget,
-    QListWidgetItem, QPushButton, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QPushButton,
+    QSizePolicy, QWidget)
 
 class Ui_FormLobby(object):
     def setupUi(self, FormLobby):
         if not FormLobby.objectName():
             FormLobby.setObjectName(u"FormLobby")
-        FormLobby.resize(525, 443)
+        FormLobby.resize(535, 230)
         self.btnCrearPartida = QPushButton(FormLobby)
         self.btnCrearPartida.setObjectName(u"btnCrearPartida")
         self.btnCrearPartida.setGeometry(QRect(80, 50, 91, 51))
@@ -31,20 +31,13 @@ class Ui_FormLobby(object):
         self.btnUnirse.setGeometry(QRect(220, 50, 101, 51))
         self.btnSalir = QPushButton(FormLobby)
         self.btnSalir.setObjectName(u"btnSalir")
-        self.btnSalir.setGeometry(QRect(80, 370, 91, 41))
+        self.btnSalir.setGeometry(QRect(220, 150, 91, 41))
         self.lineEdit = QLineEdit(FormLobby)
         self.lineEdit.setObjectName(u"lineEdit")
         self.lineEdit.setGeometry(QRect(350, 70, 113, 22))
         self.label = QLabel(FormLobby)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(370, 40, 81, 20))
-        self.listWidget = QListWidget(FormLobby)
-        self.listWidget.setObjectName(u"listWidget")
-        self.listWidget.setGeometry(QRect(120, 140, 256, 192))
-        self.pushButton = QPushButton(FormLobby)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setEnabled(False)
-        self.pushButton.setGeometry(QRect(300, 370, 75, 41))
         self.lbIdGenerado = QLabel(FormLobby)
         self.lbIdGenerado.setObjectName(u"lbIdGenerado")
         self.lbIdGenerado.setGeometry(QRect(60, 10, 121, 16))
@@ -60,7 +53,6 @@ class Ui_FormLobby(object):
         self.btnUnirse.setText(QCoreApplication.translate("FormLobby", u"Unirse a Partida", None))
         self.btnSalir.setText(QCoreApplication.translate("FormLobby", u"Salir", None))
         self.label.setText(QCoreApplication.translate("FormLobby", u"ID de la partida", None))
-        self.pushButton.setText(QCoreApplication.translate("FormLobby", u"Iniciar", None))
         self.lbIdGenerado.setText("")
     # retranslateUi
 
